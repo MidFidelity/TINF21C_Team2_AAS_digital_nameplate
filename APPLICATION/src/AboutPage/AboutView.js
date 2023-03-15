381a2229ae5e99930a0672da97def41eacc64416
ErikaZha <inf21174@lehre.dhbw-stuttgart.de>
2023-03-15 10:59:09 +0100

    Create navigation bar

---
+++ APPLICATION/src/AboutPage/AboutView.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function AboutView() {
+    return (
+        <h1>About Page</h1>
+    );
+}
+
+export default AboutView;
---
+++ APPLICATION/src/Navigation/Navbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Nav} from 'react-bootstrap';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand ms-3">
+                Nameplate Generator
+            </a>
+            <Nav className='ms-auto'>
+                <ul className="navbar-nav">
+                    <li className="nav-item active me-2">
+                        <NavLink className="nav-link" to={"/home"}> Home </NavLink>
+                    </li>
+                    <li className="nav-item me-2">
+                        <NavLink className="nav-link" to={"/about"}> About </NavLink>
+                    </li>
+                    <li className="nav-item me-3">
+                        <a className="nav-link" href="https://github.com/mk28/TINF21C_Team2_AAS_digital_nameplate">
+                            GitHub
+                        </a>
+                    </li>
+                </ul>
+            </Nav>
+        </nav>
+    );
+};
+
+export default Navbar;
