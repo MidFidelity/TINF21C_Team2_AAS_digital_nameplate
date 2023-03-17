63ff754992d99a9921c7c62c5f747e63c703df10
Janin Ahlemeyer <janin.ahlemeyer@mercedes-benz.com>
2023-03-17 20:30:17 +0100

    Expandable data view
    Added:
    - Box shadows
    - Seperate data information with lines
    - Expandable adress information

--- APPLICATION/src/AssetView/AssetData.js
+++ APPLICATION/src/AssetView/AssetData.js
@@ -6,23 +6,42 @@ const AssetData = ({data}) => {
 
     const recursiveTable = (json)=>{
         return(
-            <table>
+            <table width={"100%"}>
                 {
                 Object.entries(json).map(([key, value], index) => {
                     if (typeof value === "object"){
                       return <tr key={index}>
-                          <td><p class="categories">{key}</p></td>
-                          <td>{recursiveTable(value)}</td>
+                          {/* <td><p class="categories">{key}</p></td> */}
+
+                        <td>
+                            <div class="accordion" id="accordionExample">
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="headingOne">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    {key}
+                                </button>
+                                </h2>
+                                <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                    {recursiveTable(value)}
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+                        </td>
                       </tr>
                     }else if(key === "FilePath"){
                         return <tr key={index}>
+                            <td><img className={"ProductImage"} id={"markings"} src={value} alt={"Marking Image"}></img></td>
                             <td><p>{key}</p></td>
-                            <td><img className={"ProductImage"} src={value} alt={"Marking Image"}></img></td>
                         </tr>
                     } else{
                         return <tr key={index}>
-                            <td><p class="categories">{key}</p></td>
-                            <td><p>{value}</p></td>
+                            <td>
+                                <p class="categories">{key}</p>
+                                <p>{value}</p>
+                                <hr/>
+                            </td>
                         </tr>
                     }
                 })
