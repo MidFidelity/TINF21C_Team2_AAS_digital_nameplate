3e79b2bebef57bcb59d8777a07a830cc829fde11
mk28 <32399022+mk28@users.noreply.github.com>
2023-03-24 11:03:35 +0100

    Improve marking loading

--- APPLICATION/src/DataRetrival/DataExtractor.js
+++ APPLICATION/src/DataRetrival/DataExtractor.js
@@ -16,10 +16,10 @@ export default class DataExtractor {
                     returnObject[nameplateElement.idShort] = this.getLangStringValue(nameplateElement.value)
                     break;
                 case "SubmodelElementCollection":
-                    if(nameplateElement.idShort.match(/[Mm]arking[_\-\w]*/)){
+                    if(nameplateElement.idShort.match(/(?!Markings)[Mm]arking[\-\w]*/ug)){
                         if(!("Markings" in returnObject))returnObject["Markings"]={};
                         returnObject["Markings"][nameplateElement.idShort] = this.extractAllDataV1(baseUrl, nameplateElement.value, path + (path.length > 0 ? "." : "") + nameplateElement.idShort)
-                        returnObject["Markings"][nameplateElement.idShort]["MarkingName"] = nameplateElement.idShort
+                        if(!"MarkingName" in returnObject["Markings"][nameplateElement.idShort])returnObject["Markings"][nameplateElement.idShort]["MarkingName"] = nameplateElement.idShort
                     }else{
                         returnObject[nameplateElement.idShort] = this.extractAllDataV1(baseUrl, nameplateElement.value, path + (path.length > 0 ? "." : "") + nameplateElement.idShort)
                     }
@@ -47,7 +47,13 @@ export default class DataExtractor {
                     returnObject[nameplateElement.idShort] = this.getLangStringValue(nameplateElement.value)
                     break;
                 case "SubmodelElementCollection":
-                    returnObject[nameplateElement.idShort] = this.extractAllDataV3(baseUrl, nameplateElement.value, path + (path.length > 0 ? "." : "") + nameplateElement.idShort)
+                    if(nameplateElement.idShort.match(/(?!Markings)[Mm]arking[\-\w]*/ug)){
+                        if(!("Markings" in returnObject))returnObject["Markings"]={};
+                        returnObject["Markings"][nameplateElement.idShort] = this.extractAllDataV3(baseUrl, nameplateElement.value, path + (path.length > 0 ? "." : "") + nameplateElement.idShort)
+                        if(!"MarkingName" in returnObject["Markings"][nameplateElement.idShort])returnObject["Markings"][nameplateElement.idShort]["MarkingName"] = nameplateElement.idShort
+                    }else{
+                        returnObject[nameplateElement.idShort] = this.extractAllDataV3(baseUrl, nameplateElement.value, path + (path.length > 0 ? "." : "") + nameplateElement.idShort)
+                    }
                     break;
                 case "Property":
                     returnObject[nameplateElement.idShort] = nameplateElement.value
