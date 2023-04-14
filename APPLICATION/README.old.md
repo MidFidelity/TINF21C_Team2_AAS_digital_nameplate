a28e976e92c26e5fe313939b636a3c5d5313d54b
Marcel Hintze <96146029+mercal03@users.noreply.github.com>
2023-04-14 10:23:25 +0200

    Mika bitte lösch alles ihr seit zu gut

--- APPLICATION/README.old.md
+++
@@ -1,2 +0,0 @@
-# Code for the webapp
-⚠ Code is work in Progress ⚠
--- APPLICATION/src/AboutPage/AboutView.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function AboutView() {
-    return (
-        <h1>About Page</h1>
-    );
-}
-
-export default AboutView;
