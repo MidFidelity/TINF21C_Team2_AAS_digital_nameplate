fa480871cc0356d05555fb7c907ac110cab83b66
mk28 <32399022+mk28@users.noreply.github.com>
2023-04-17 20:17:41 +0200

    Murrelektronik server working

--- APPLICATION/src/DataRetrival/API.js
+++ APPLICATION/src/DataRetrival/API.js
@@ -1,37 +1,40 @@
-function submodelDataPathV1(aasIdentifier, submodelIdentifier) {
-    return `shells/${aasIdentifier}/aas/submodels/${submodelIdentifier}/submodel`
+function submodelPathsV1(aasIdentifier, submodelIdentifier) {
+    return [
+        {
+            submodel: `shells/${aasIdentifier}/aas/submodels/${submodelIdentifier}/submodel`,
+            submodelElements: `shells/${aasIdentifier}/aas/submodels/${submodelIdentifier}/submodel/submodel-elements`
+        },
+        {
+            submodel: `submodels/${submodelIdentifier}`,
+            submodelElements: `submodels/${submodelIdentifier}/submodel/submodel-elements`
+        },
+        {
+            submodel: `shells/${encodeURIComponent(window.atob(aasIdentifier))}/aas/submodels`,
+            submodelElements: `shells/${encodeURIComponent(window.atob(aasIdentifier))}/aas/submodels`
+        }
+    ]
 }
-
-function submodelDataPathV3(aasIdentifier, submodelIdentifier) {
-    return `shells/${aasIdentifier}/submodels/${submodelIdentifier}/submodel`
-}
-
-function submodelFallbackPathV1_1(aasIdentifier, submodelIdentifier) {
-    return `shells/${aasIdentifier}/aas/submodels/${submodelIdentifier}/submodel`
-}
-
-function submodelElementsPathV1(aasIdentifier, submodelIdentifier){
-    return `shells/${aasIdentifier}/aas/submodels/${submodelIdentifier}/submodel/submodel-elements`
-}
-
-function submodelElementsPathV3(aasIdentifier, submodelIdentifier){
-    return `shells/${aasIdentifier}/submodels/${submodelIdentifier}/submodel/submodelelements`
+function submodelPathsV3(aasIdentifier, submodelIdentifier) {
+    return [
+        {
+            submodel: `shells/${aasIdentifier}/submodels/${submodelIdentifier}/submodel`,
+            submodelElements: `shells/${aasIdentifier}/submodels/${submodelIdentifier}/submodel/submodelelements`
+        },
+        {
+            submodel: `submodels/${submodelIdentifier}`,
+            submodelElements: `submodels/${submodelIdentifier}/submodelelements`
+        }
+    ]
 }
 
 function addressShellList() {
     return "shells";
 }
 
-function imageBasePathV1(submodelIdEncoded){
-    return `submodels/${submodelIdEncoded}/submodel/submodel-elements`
-}
-
-function imageBasePathV3(submodelIdEncoded){
-    return `submodels/${submodelIdEncoded}/submodelelements`
-}
-
 export {
-    addressShellList, imageBasePathV1, imageBasePathV3, submodelElementsPathV1, submodelElementsPathV3, submodelDataPathV1, submodelDataPathV3
+    addressShellList,
+    submodelPathsV1,
+    submodelPathsV3
 }
 
 
--- APPLICATION/src/DataRetrival/DataRefinery.js
+++ APPLICATION/src/DataRetrival/DataRefinery.js
@@ -1,12 +1,8 @@
 import DataExtractor from "./DataExtractor.js";
 import {
     addressShellList,
-    imageBasePathV1,
-    imageBasePathV3,
-    submodelDataPathV1,
-    submodelDataPathV3,
-    submodelElementsPathV1,
-    submodelElementsPathV3,
+    submodelPathsV1,
+    submodelPathsV3
 } from "./API";
 import {object} from "prop-types";
 
@@ -19,21 +15,6 @@ export default class DataRefinery {
             this.serverBaseAddress = serverBaseAddress + "/";
         }
         this.apiVersion = undefined;
-    }
-
-    async #loadDependencies_old() {
-        if (!this.fullSubmodelList) {
-            await this.#getDataFromServer(this.serverBaseAddress).then(result => this.fullSubmodelList = result)
-        }
-        if (!this.apiVersion) {
-            this.analyzeApiVersion()
-        }
-        if (!this.nameplateSubmodels) {
-            this.nameplateSubmodels = this.#findAllSubmodels_old("Nameplate")
-        }
-        if (!this.technicalDataSubmodels) {
-            this.technicalDataSubmodels = this.#findAllSubmodels_old("TechnicalData")
-        }
     }
 
     async getAPIVersion() {
@@ -64,37 +45,55 @@ export default class DataRefinery {
                                     let submodelIdEncoded = window.btoa(submodelId)
                                     let apiVersion = this.analyzeApiVersion(submodel)
 
-                                    let submodelElementsPath
-                                    let submodelDataPath
+                                    let submodelPaths
                                     if (apiVersion === 3) {
-                                        submodelElementsPath = submodelElementsPathV3(assetIdEncoded, submodelIdEncoded)
-                                        submodelDataPath = submodelDataPathV3(assetIdEncoded, submodelIdEncoded)
+                                        submodelPaths = submodelPathsV3(assetIdEncoded, submodelIdEncoded)
                                     } else {
-                                        submodelElementsPath = submodelElementsPathV1(assetIdEncoded, submodelIdEncoded)
-                                        submodelDataPath = submodelDataPathV1(assetIdEncoded, submodelIdEncoded)
+                                        submodelPaths = submodelPathsV1(assetIdEncoded, submodelIdEncoded)
                                     }
-
-                                    let submodelData = await this.#getDataFromServer(this.serverBaseAddress + submodelDataPath)
-                                        .then((result) => {
-                                            let submodelName = result.idShort
-
-                                            let extractedSubmodelData
-                                            let de = new DataExtractor(result["submodelElements"])
-                                            if (apiVersion === 3) {
-                                                extractedSubmodelData = de.extractAllDataV3(this.serverBaseAddress + submodelElementsPath)
-                                            } else {
-                                                extractedSubmodelData = de.extractAllDataV1(this.serverBaseAddress + submodelElementsPath)
-                                            }
-
-                                            return {
-                                                [submodelName]: {
-                                                    idShort: submodelName,
-                                                    id: submodelId,
-                                                    idEncoded: submodelIdEncoded,
-                                                    ...extractedSubmodelData
+                                    let submodelData
+                                    for (const submodelPath of submodelPaths) {
+                                        submodelData = await this.#getDataFromServer(this.serverBaseAddress + submodelPath.submodel, true)
+                                            .then((result) => {
+                                                let submodelDataArray
+                                                let i = 0
+                                                if (!result) return undefined
+                                                if (Array.isArray(result)) {
+                                                    submodelDataArray = result
+                                                }else {
+                                                    submodelDataArray = [result]
                                                 }
-                                            }
-                                        })
+                                                let returnData = {}
+                                                do {
+                                                    let submodelName = submodelDataArray[i].idShort
+
+                                                    let extractedSubmodelData
+                                                    let de = new DataExtractor(submodelDataArray[i]["submodelElements"])
+                                                    if (apiVersion === 3) {
+                                                        extractedSubmodelData = de.extractAllDataV3(this.serverBaseAddress + submodelPath.submodelElements)
+                                                    } else {
+                                                        extractedSubmodelData = de.extractAllDataV1(this.serverBaseAddress + submodelPath.submodelElements)
+                                                    }
+
+
+                                                    returnData = {
+                                                        ...returnData,
+                                                        [submodelName]: {
+                                                            idShort: submodelName,
+                                                            id: submodelId,
+                                                            idEncoded: submodelIdEncoded,
+                                                            ...extractedSubmodelData
+                                                        }
+                                                    }
+                                                    i++
+                                                }while (i<submodelDataArray.length)
+                                                return returnData
+                                            })
+                                        if (submodelData) {
+                                            break;
+                                        }
+                                        console.warn("Using fallback")
+                                    }
                                     resolve(submodelData)
                                 } catch (e) {
                                     console.error(e)
@@ -112,98 +111,16 @@ export default class DataRefinery {
 
                     Promise.all(submodels).then((result) => {
                         result.forEach((submodel) => {
-                            assetObject[Object.keys(submodel)[0]]=submodel[Object.keys(submodel)[0]]
+                            assetObject[Object.keys(submodel)[0]] = submodel[Object.keys(submodel)[0]]
+                            if (Object.keys(submodel)[0]==="TechnicalData"){
+                                assetObject.productImages =  this.searchForKey(submodel[Object.keys(submodel)[0]], /[pP]roductImage\d*/)
+                            }
                         })
                     })
 
                     return assetObject
                 })
             })
-    }
-
-    async getFullAASList_old() {
-        await this.#loadDependencies_old();
-        return this.#getDataFromServer(this.serverBaseAddress + addressShellList()).then(response => {
-            if (!response || (Object.hasOwn(response, 'success/') && !response.success)) {
-                throw new Error(this.serverBaseAddress + addressShellList())
-            }
-            console.log(response);
-            let returnData = response.map((obj, index) => {
-                let id = obj["identification"] ? obj["identification"]["id"] : obj["id"]
-                let nameplateData
-                let technicalData
-                if (obj["submodels"]) {
-                    nameplateData = this.nameplateSubmodels.find((item) => {
-                        return obj["submodels"].find((sub) => {
-                            return item.id === sub.keys[0].value
-                        })
-                    });
-                    technicalData = this.technicalDataSubmodels.find((item) => {
-                        return obj["submodels"].find((sub) => {
-                            return item.id === sub.keys[0].value
-                        })
-                    });
-                }
-
-                let productImages = technicalData ? this.searchForKey(technicalData, /[pP]roductImage\d*/) : []
-
-                return {
-                    "idShort": obj["idShort"],
-                    "id": id,
-                    "idEncoded": window.btoa(id),
-                    "nameplateId": nameplateData ? nameplateData.id : null,
-                    "nameplateIdEncoded": nameplateData ? nameplateData.idEncoded : null,
-                    "num": index,
-                    "productImages": productImages,
-                    "nameplate": nameplateData ? nameplateData : null
-                }
-            });
-            console.log(returnData)
-            return returnData
-        }).catch(err => {
-            console.warn(err);
-            return [];
-        });
-    }
-
-    #findAllSubmodels_old(name) {
-        console.log("Submodels:")
-        console.log(this.fullSubmodelList);
-        let filteredResult = this.fullSubmodelList.filter((item) => {
-            return item.idShort ? item.idShort === name : false
-        }).map((item) => {
-
-
-            let nameplate
-            let ids
-            switch (this.apiVersion) {
-                case 1:
-                    ids = {
-                        idShort: item.idShort,
-                        id: item.identification.id,
-                        idEncoded: window.btoa(item.identification.id)
-                    }
-                    nameplate = new DataExtractor(item["submodelElements"]).extractAllDataV1(this.serverBaseAddress + imageBasePathV1(ids.idEncoded))
-                    break;
-                case 3:
-                    ids = {
-                        idShort: item.idShort,
-                        id: item.id,
-                        idEncoded: window.btoa(item.id)
-                    }
-                    nameplate = new DataExtractor(item["submodelElements"]).extractAllDataV3(this.serverBaseAddress + imageBasePathV3(ids.idEncoded))
-                    break;
-                default:
-                    break;
-            }
-            return {
-                ...ids,
-                ...nameplate
-            }
-        })
-        console.log("Filtered Submodels:")
-        console.log(filteredResult);
-        return filteredResult;
     }
 
     searchForKey(json, regex) {
@@ -229,53 +146,30 @@ export default class DataRefinery {
         return apiVersion
     }
 
-    analyzeApiVersion_old() {
-        if (!this.fullSubmodelList) return;
-        let submodel = this.fullSubmodelList[0];
-        let modelType = submodel["modelType"];
-        if (modelType === "Submodel") {
-            this.apiVersion = 3
-        } else if (modelType["name"] === "Submodel") {
-            this.apiVersion = 1
-        } else {
-            this.apiVersion = -1
-        }
-        console.log("API-Version: " + this.apiVersion)
-        window.dispatchEvent(new CustomEvent("apiVersionSet", {detail: {apiVersion: this.apiVersion}}))
-    }
-
-
-    async #getDataFromServer_new(address) {
-        console.log("Making request to " + address);
-        return fetch(address)
-            .then(result => {
-                return result.json().then((jsonData) => (jsonData))
-            })
-            .catch(err => {
-                console.log({success: false, text: err})
-                this.apiVersion = -1
-                window.dispatchEvent(new CustomEvent("apiVersionSet", {detail: {apiVersion: this.apiVersion}}))
-            });
-    }
-
-    async #getDataFromServer(address) {
+    async #getDataFromServer(address, silent = false) {
         console.log("Making request to " + address);
         return fetch(address)
             .then(response => {
                 if (!response.ok) {
-                    console.error("Fetch not successful")
-                    return {}
+                    if (!silent) {
+                        console.error("Fetch not successful")
+                    }
+                    return undefined
                 }
+
                 return response.json().then(jsonResponse => {
                     return jsonResponse;
                 }).catch(err => {
-                    console.log(response, err)
+                    console.warn(response, err)
                 })
             })
             .catch(err => {
-                console.log({success: false, text: err})
-                this.apiVersion = -1
-                window.dispatchEvent(new CustomEvent("apiVersionSet", {detail: {apiVersion: this.apiVersion}}))
+                if (!silent) {
+                    console.log({success: false, text: err})
+                    this.apiVersion = -1
+                    window.dispatchEvent(new CustomEvent("apiVersionSet", {detail: {apiVersion: this.apiVersion}}))
+                }
+                return undefined
             });
     }
 }
